4dedcb38feb6577bad65019d084202a6176c8056
konsultera <iamgauravpk@gmail.com>
2018-09-20 18:55:55 +0530

    master & audit

--- frmMenu.Designer.vb
+++ frmMenu.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class frmMenu
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,37 +20,58 @@ Partial Class frmMenu
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.components = New System.ComponentModel.Container()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btn_Master = New System.Windows.Forms.GroupBox()
+        Me.btn_Auditing = New System.Windows.Forms.Button()
+        Me.btn_Push = New System.Windows.Forms.Button()
         Me.CMBPORTNO = New System.Windows.Forms.ComboBox()
         Me.btnExit = New System.Windows.Forms.Button()
         Me.BtnErase = New System.Windows.Forms.Button()
         Me.btnDownLoad = New System.Windows.Forms.Button()
         Me.btnSendFile = New System.Windows.Forms.Button()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.IoPort = New System.IO.Ports.SerialPort(Me.components)
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
-        Me.IOPORT_DW = New System.IO.Ports.SerialPort(Me.components)
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.txtProcStat = New System.Windows.Forms.TextBox()
-        Me.GroupBox1.SuspendLayout()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btn_Master.SuspendLayout()
         Me.SuspendLayout()
         '
-        'GroupBox1
+        'btn_Master
         '
-        Me.GroupBox1.Controls.Add(Me.CMBPORTNO)
-        Me.GroupBox1.Controls.Add(Me.btnExit)
-        Me.GroupBox1.Controls.Add(Me.BtnErase)
-        Me.GroupBox1.Controls.Add(Me.btnDownLoad)
-        Me.GroupBox1.Controls.Add(Me.btnSendFile)
-        Me.GroupBox1.Location = New System.Drawing.Point(67, 68)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(241, 296)
-        Me.GroupBox1.TabIndex = 0
-        Me.GroupBox1.TabStop = False
+        Me.btn_Master.Controls.Add(Me.Button1)
+        Me.btn_Master.Controls.Add(Me.btn_Auditing)
+        Me.btn_Master.Controls.Add(Me.btn_Push)
+        Me.btn_Master.Controls.Add(Me.CMBPORTNO)
+        Me.btn_Master.Controls.Add(Me.btnExit)
+        Me.btn_Master.Controls.Add(Me.BtnErase)
+        Me.btn_Master.Controls.Add(Me.btnDownLoad)
+        Me.btn_Master.Controls.Add(Me.btnSendFile)
+        Me.btn_Master.Location = New System.Drawing.Point(67, 68)
+        Me.btn_Master.Name = "btn_Master"
+        Me.btn_Master.Size = New System.Drawing.Size(241, 328)
+        Me.btn_Master.TabIndex = 0
+        Me.btn_Master.TabStop = False
+        '
+        'btn_Auditing
+        '
+        Me.btn_Auditing.Location = New System.Drawing.Point(130, 284)
+        Me.btn_Auditing.Name = "btn_Auditing"
+        Me.btn_Auditing.Size = New System.Drawing.Size(86, 29)
+        Me.btn_Auditing.TabIndex = 6
+        Me.btn_Auditing.Text = "Auditing"
+        Me.btn_Auditing.UseVisualStyleBackColor = True
+        '
+        'btn_Push
+        '
+        Me.btn_Push.Location = New System.Drawing.Point(23, 284)
+        Me.btn_Push.Name = "btn_Push"
+        Me.btn_Push.Size = New System.Drawing.Size(10, 29)
+        Me.btn_Push.TabIndex = 5
+        Me.btn_Push.Text = "Push"
+        Me.btn_Push.UseVisualStyleBackColor = True
+        Me.btn_Push.Visible = False
         '
         'CMBPORTNO
         '
@@ -108,31 +129,15 @@ Partial Class frmMenu
         Me.btnSendFile.Text = "Send File"
         Me.btnSendFile.UseVisualStyleBackColor = False
         '
-        'TextBox2
-        '
-        Me.TextBox2.Location = New System.Drawing.Point(331, 68)
-        Me.TextBox2.Multiline = True
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(233, 296)
-        Me.TextBox2.TabIndex = 5
-        Me.TextBox2.Visible = False
-        '
-        'IoPort
-        '
-        '
-        'OpenFileDialog1
-        '
-        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
-        '
         'Label2
         '
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.Label2.Location = New System.Drawing.Point(12, 37)
+        Me.Label2.Location = New System.Drawing.Point(1, 37)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(360, 28)
+        Me.Label2.Size = New System.Drawing.Size(358, 28)
         Me.Label2.TabIndex = 7
-        Me.Label2.Text = "Version 2.03.15 "
+        Me.Label2.Text = "Version 2.0 Rel. Oct. 2017"
         Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Label1
@@ -156,37 +161,44 @@ Partial Class frmMenu
         Me.txtProcStat.Size = New System.Drawing.Size(359, 15)
         Me.txtProcStat.TabIndex = 8
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(38, 284)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(86, 29)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Master"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'frmMenu
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
-        Me.ClientSize = New System.Drawing.Size(323, 446)
+        Me.ClientSize = New System.Drawing.Size(348, 408)
         Me.Controls.Add(Me.txtProcStat)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.GroupBox1)
-        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.btn_Master)
         Me.Name = "frmMenu"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "PC2M"
-        Me.GroupBox1.ResumeLayout(False)
+        Me.btn_Master.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents btn_Master As System.Windows.Forms.GroupBox
     Friend WithEvents btnExit As System.Windows.Forms.Button
     Friend WithEvents BtnErase As System.Windows.Forms.Button
     Friend WithEvents btnDownLoad As System.Windows.Forms.Button
     Friend WithEvents btnSendFile As System.Windows.Forms.Button
-    Friend WithEvents IoPort As System.IO.Ports.SerialPort
     Friend WithEvents CMBPORTNO As System.Windows.Forms.ComboBox
     Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
-    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
-    Friend WithEvents IOPORT_DW As System.IO.Ports.SerialPort
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents txtProcStat As System.Windows.Forms.TextBox
-
+    Friend WithEvents btn_Push As Button
+    Friend WithEvents btn_Auditing As Button
+    Friend WithEvents Button1 As Button
 End Class
--- frmMenu.vb
+++ frmMenu.vb
@@ -1,73 +1,31 @@
-﻿Public Class frmMenu
-    Dim strdata As String
-    Dim NoofRecords As Long
-    Dim strSysdateTime As String
-    Dim tFileName As String
-    Dim objReader As System.IO.StreamReader
-    Dim tStr As String
-    Dim tLenCntr As Int64
-    Dim tAckChr As String
-    Dim tDataCntr As Int64
-    Dim tDwnData As String
-    Dim tRecCntr As Int64
-    Public Declare Sub Sleep Lib "kernel32.dll" (ByVal dwMilliseconds As Long)
+﻿Imports PC2M.PC2M
+Imports MySql.Data.MySqlClient
+Imports System.IO
+
+Public Class frmMenu
+
+    Dim conn As New MySqlConnection
+    Public dbcomm As MySqlCommand
+    Public dbread As MySqlDataReader
 
     Private Sub btnSendFile_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSendFile.Click
         Dim strSendFileName As String
-        Dim tChar As String
+
         OpenFileDialog1.ShowDialog()
+
         strSendFileName = OpenFileDialog1.FileName
+
         If strSendFileName.Length > 1 Then
             Try
-                tLenCntr = 1
-                tDataCntr = 1
-                'Call ReadIni()
-                tRecCntr = 1
-                txtProcStat.Text = ""
-                With IoPort
-                    If .IsOpen Then .Close()
-                    .PortName = CMBPORTNO.Text
-                    .Handshake = IO.Ports.Handshake.None
-                    .RtsEnable = True
-                    .WriteBufferSize = 512
-                    .BaudRate = 115200
-                    .Parity = IO.Ports.Parity.None
-                    .ParityReplace = 8
-                    .StopBits = 1
-                    If System.IO.File.Exists(strSendFileName) = True Then
-                        tStr = ""
-                        objReader = New System.IO.StreamReader(strSendFileName, System.Text.UnicodeEncoding.UTF8)
-                        txtProcStat.Text = "Please Wait !!! Process Text file..."
-                        tStr = objReader.ReadToEnd
-                        'Do While objReader.Peek() <> -1
-                        '    tChar = Chr(objReader.Read)
+                Dim tSendFile As New PC2M
+                Call tSendFile.UploadData(strSendFileName, CMBPORTNO.Text)
 
-                        '    tStr = tStr & tChar
-                        '    If tChar = "@" Then tLenCntr = tLenCntr + 1
-                        '    Application.DoEvents()
-                        'Loop
-                        objReader.Close()
-                        txtProcStat.Text = "Text File Process Completed... Starting Data Send to HHD..."
-                        MsgBox("Make a HHD On!!!", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "PC2M")
-
-
-                        '  If Asc(Mid(tStr, 1, 1)) = 239 Then
-                        'tStr = Mid(tStr, 6, Len(tStr))
-                        'End If
-                        tLenCntr = 1
-                        tAckChr = Mid(tStr, 2, 1)
-                        tStr = Mid(tStr, 3, Len(tStr))
-                        .Open()
-                        .Write("$")
-                    Else
-                        MsgBox("File Does Not Exist")
-                    End If
-                End With
             Catch ex As Exception
                 MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error)
             End Try
         End If
     End Sub
+
     Private Sub frmMenu_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = False
         For Each sp As String In My.Computer.Ports.SerialPortNames
@@ -75,131 +33,144 @@
         Next
     End Sub
 
-    Private Sub IoPort_DataReceived(ByVal sender As Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles IoPort.DataReceived
-        Dim encodedString() As Byte
-        Dim tBuffer As String
-        Dim tFname As String
-        Dim tCntr As Int64
-        Dim tLCntr As Int64
-        Dim tUCntr As Int64
-        Dim tSendString As String
-
-        Try
-            With IoPort
-                tBuffer = Chr(.ReadChar)
-                TextBox2.Text = TextBox2.Text & "Receive String : " & tBuffer
-                If tBuffer.StartsWith("$") Then
-                    'TextBox1.Text = tBuffer
-                    If Len(tAckChr) <> 0 Then
-                        .Write(tAckChr)
-                        TextBox2.Text = TextBox2.Text & "Sending String : " & tAckChr & vbCrLf
-                        tAckChr = ""
-                    End If
-                ElseIf tBuffer.StartsWith("O") Then
-                    'TextBox1.Text = tBuffer
-                    If tDataCntr >= Len(tStr) Then
-                        .Write(Chr(4))
-                        Sleep(300)
-                        .Close()
-                        'System.IO.File.AppendAllText(My.Application.Info.DirectoryPath & "\Send.txt", TextBox2.Text)
-                        MsgBox("Process Completed Successfully!!!", MsgBoxStyle.OkOnly)
-                        txtProcStat.Text = ""
-                    Else
-                        tSendString = ""
-                        For tCntr = tDataCntr To Len(tStr)
-                            tSendString = tSendString & Mid(tStr, tCntr, 1)
-                            If Mid(tStr, tCntr, 1) = "@" Then
-                                Exit For
-                            End If
-                        Next tCntr
-                        tDataCntr = tCntr + 1
-
-                        tSendString = tSendString.Replace(vbCrLf, "")
-                        tSendString = tSendString.Replace(vbCr, "")
-
-                        TextBox2.Text = tSendString
-                        encodedString = encode(tSendString)
-                        .Write(encodedString, 0, encodedString.Length)
-                        TextBox2.Text = "Send String :" & encodedString.ToString
-                        Call ProStat()
-                        tLenCntr = tLenCntr + 1
-                        tRecCntr = tRecCntr + 1
-                        If tSendString = Chr(4) Then
-                            Sleep(300)
-                            .Close()
-                            MsgBox("Process Completed Successfully!!!", MsgBoxStyle.OkOnly)
-                            txtProcStat.Text = ""
-                        End If
-                    End If
-                End If
-
-            End With
-        Catch ex As Exception
-            MsgBox(ex.Message, MsgBoxStyle.OkOnly)
-            txtProcStat.Text = ""
-            If IoPort.IsOpen Then IoPort.Close()
-        End Try
-    End Sub
-    Private Sub ProStat()
-        txtProcStat.Text = "Please Wait..... Loading Record No.: " & tRecCntr
-    End Sub
     Private Sub btnDownLoad_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDownLoad.Click
-        frmDownload.ShowDialog()
-    End Sub
-    Private Sub BtnErase_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnErase.Click
-        Dim strSendFileName As String
-        strSendFileName = System.String.Concat(My.Application.Info.DirectoryPath, "\Erase.txt")
-        If strSendFileName.Length > 1 Then
+        Dim strRecFile As String
+        Dim objReader As System.IO.StreamReader
+        Dim tStr As String
+        If Not IO.Directory.Exists(My.Application.Info.DirectoryPath & "\download") Then IO.Directory.CreateDirectory(My.Application.Info.DirectoryPath & "\download")
+        strRecFile = System.String.Concat(My.Application.Info.DirectoryPath, "\download\outputdata" & Format(Now, "dd_MM_yyyy_hh_mm") & ".txt")
+        If strRecFile.Length > 1 Then
             Try
-                tRecCntr = 1
-                tLenCntr = 1
-                tDataCntr = 1
-                With IoPort
-                    If .IsOpen Then .Close()
-                    .PortName = CMBPORTNO.Text
-                    .Handshake = IO.Ports.Handshake.None
-                    .RtsEnable = True
-                    .WriteBufferSize = 512
-                    .BaudRate = 115200
-                    .Parity = IO.Ports.Parity.None
-                    .ParityReplace = 8
-                    .StopBits = 1
-                    If System.IO.File.Exists(strSendFileName) = True Then
-                        tStr = ""
-                        objReader = New System.IO.StreamReader(strSendFileName)
-                        Do While objReader.Peek() <> -1
-                            tStr = tStr & objReader.ReadLine
-                            tLenCntr = tLenCntr + 1
-
-                            Application.DoEvents()
-                        Loop
-                        objReader.Close()
-                        tLenCntr = 1
-                        tAckChr = Mid(tStr, 2, 1)
-
-                        tStr = Mid(tStr, 3, Len(tStr))
-                        'tStr = Space(2) & tStr
-                        .Open()
-                        .Write("$")
-                    Else
-                        MsgBox("File Does Not Exist")
+                Dim tGetFile As New PC2M
+                tGetFile.DownLoad(strRecFile, "B", CMBPORTNO.Text)
+                If IO.File.Exists(System.String.Concat(My.Application.Info.DirectoryPath, "\download\outputdata" & Format(Now, "dd_MM_yyyy_hh_mm") & ".txt")) Then
+                    If IO.File.Exists(System.String.Concat(My.Application.Info.DirectoryPath, "\download\outputdata" & Format(Now, "dd_MM_yyyy_hh_mm") & ".csv")) Then
+                        IO.File.Delete(System.String.Concat(My.Application.Info.DirectoryPath, "\download\outputdata" & Format(Now, "dd_MM_yyyy_hh_mm") & ".csv"))
                     End If
-                End With
+                    objReader = New System.IO.StreamReader(strRecFile)
+                    tStr = Replace(objReader.ReadToEnd, "@", "")
+                    objReader.Close()
+                    IO.File.AppendAllText(System.String.Concat(My.Application.Info.DirectoryPath, "\download\outputdata" & Format(Now, "dd_MM_yyyy_hh_mm") & ".csv"), tStr)
+                End If
             Catch ex As Exception
                 MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error)
             End Try
         End If
     End Sub
+
+    Private Sub BtnErase_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnErase.Click
+        Dim strSendFileName As String
+        strSendFileName = System.String.Concat(My.Application.Info.DirectoryPath, "\Erase.txt")
+        If strSendFileName.Length > 1 Then
+            Try
+                Dim tSendFile As New PC2M
+                tSendFile.UploadData(strSendFileName, CMBPORTNO.Text)
+            Catch ex As Exception
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+    End Sub
+
     Private Sub btnExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExit.Click
         Me.Close()
     End Sub
-    Public Shared Function encode(ByVal str As String) As Byte()
-        Dim utf8Encoding As New System.Text.UTF8Encoding
-        Dim encodedString() As Byte
 
-        encodedString = utf8Encoding.GetBytes(str)
+    'For Database Connection
+    Public Sub DBconnect()
+        Dim DatabaseName As String = "db_vvmt_ticketing"
+        Dim server As String = "192.168.1.253"
+        Dim userName As String = "root"
+        Dim password As String = "mysql@123"
+        Dim test As String = ""
+        If Not conn Is Nothing Then conn.Close()
+        conn.ConnectionString = String.Format("server={0}; user id={1}; password={2}; database={3}; pooling=false", server, userName, password, DatabaseName)
+        Try
+            conn.Open()
+            MsgBox("Connected")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        conn.Close()
+    End Sub
 
-        Return encodedString
-    End Function
-    
+    'Read Download File And Push Into DataBase
+    Private Sub ReadFile()
+        Dim txtFilePath As String = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase)
+        Dim txtpath As String = txtFilePath & "\" + "download"
+        Dim fileEntries As String() = Directory.GetFiles("download", "*.txt")
+        Dim fileNames As String
+        Dim TextLine As String
+        Dim Sql As String
+
+        For Each fileNames In fileEntries
+            If System.IO.File.Exists(fileNames) = True Then
+                Dim objReader As New System.IO.StreamReader(fileNames)
+                Do While objReader.Peek() <> -1
+                    TextLine = objReader.ReadLine()
+                    Dim words As String() = TextLine.Split(New Char() {" "c})
+                    Dim word As String
+                    For Each word In words
+                        Dim WayBill_No As String = words(0).ToString()
+                        Dim Word_2 As String = words(1).ToString()
+                        Dim ABC As String = "0487526"
+                        Sql = "SELECT fldv_waybill_no FROM tbl_waybill_mst WHERE fldv_waybill_no = '" + WayBill_No + "'"
+                        Try
+                            conn.Open()
+                            dbcomm = New MySqlCommand(Sql, conn)
+                            dbread = dbcomm.ExecuteReader()
+                            While dbread.Read
+
+                            End While
+                            conn.Close()
+                        Catch ex As Exception
+                            MsgBox("unable to add" + ex.Message)
+
+                        End Try
+                    Next
+                Loop
+                'Textbox1.Text = TextLine
+            Else
+                MessageBox.Show("File Does Not Exist")
+            End If
+        Next
+    End Sub
+
+    'btn_Push
+    Private Sub btn_Push_Click(sender As Object, e As EventArgs) Handles btn_Push.Click
+        'ReadFile()
+        'getConductorDetails()
+
+    End Sub
+
+    Public Sub getConductorDetails()
+        Dim _fldi_conductor_id As String
+        Dim Sql As String
+
+        Sql = "select fldi_conductor_id,fldv_conductor_name,fldv_employee_code  from tbl_conductor_mst where fldc_st" +
+            "atus ='Y' order by fldv_employee_code"
+        Try
+            conn.Open()
+            dbcomm = New MySqlCommand(Sql, conn)
+            dbread = dbcomm.ExecuteReader()
+            While dbread.Read()
+                _fldi_conductor_id = dbread.GetString("fldi_conductor_id")
+            End While
+            conn.Close()
+        Catch ex As Exception
+            MsgBox("unable to add" + ex.Message)
+        End Try
+    End Sub
+
+    'btn_Auditing
+    Private Sub btn_Auditing_Click(sender As Object, e As EventArgs) Handles btn_Auditing.Click
+        Dim Auditing = New frm_Auditing
+        Auditing.Show()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Master = New frm_Master
+        Master.Show()
+    End Sub
 End Class
+
+
